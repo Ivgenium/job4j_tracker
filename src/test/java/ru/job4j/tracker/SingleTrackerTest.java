05fc4ed25ae15d83ae0d5503936be0bb5b97eb86
Ivgenium <genrymorgon@mail.ru>
2025-02-10 01:41:30 +0300

    Singleton [#94606]

---
+++ src/test/java/ru/job4j/tracker/SingleTrackerTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingleTrackerTest {
+    @Test
+    public void whenSingletonThenItemsAreInTheSameTracker() {
+        SingleTracker singleTracker1 = SingleTracker.getInstance();
+        Item test1 = new Item();
+        test1.setName("test1");
+        singleTracker1.add(test1);
+        SingleTracker singleTracker2 = SingleTracker.getInstance();
+        Item test2 = new Item();
+        test2.setName("test2");
+        singleTracker2.add(test2);
+        Item[] result = SingleTracker.getInstance().findAll();
+        Item[] expected = new Item[] {test1, test2};
+        assertThat(result).isEqualTo(expected);
+    }
+}
