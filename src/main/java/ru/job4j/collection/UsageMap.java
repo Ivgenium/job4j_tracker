6847c3bf11316465cd8ae3bf8ddb4a752cee2d44
Ivgenium <genrymorgon@mail.ru>
2025-02-16 20:18:20 +0300

    Map, HashMap [#212652 #532746]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,7 +7,11 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("alexg@yandex.ru", "Alexander Grishin");
+        hashMap.put("zverevvasya@mail.ru", "Vasiliy Alibabaevich");
         hashMap.put("ivannosov@mail.ru", "Ivan Nosov");
+        hashMap.put("zverevvasya@mail.ru", "Vasiliy Zverev");
+        hashMap.put("vladsidorov@inbox.ru", "Vladimir Sidorov");
+        hashMap.put("alexg@yandex.ru", "Alexander Grishkin");
         for (Map.Entry<String, String> entry : hashMap.entrySet()) {
             System.out.println("mail: " + entry.getKey() + " user: " + entry.getValue());
         }
