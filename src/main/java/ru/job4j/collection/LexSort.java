94fd7296cc0e9055c067989926aa5583797101d8
Ivgenium <genrymorgon@mail.ru>
2025-02-17 21:59:21 +0300

    Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int leftInt = Integer.parseInt(left.split("\\.")[0]);
+        int rightInt = Integer.parseInt(right.split("\\.")[0]);
+        int rsl = Integer.compare(leftInt, rightInt);
+        return rsl != 0 ? rsl : left.split("\\.")[1].compareTo(right.split("\\.")[1]);
+    }
+}
